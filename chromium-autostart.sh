27100c018c18b8c06439770a6f66065dd718bf80
老竭力 <jerrykuku@qq.com>
2022-12-22 22:58:51 +0800

    Update chromium-autostart.sh

--- chromium-autostart.sh
+++ chromium-autostart.sh
@@ -6,7 +6,7 @@ RES_Y=$(< /sys/class/graphics/fb0/modes cut -d"x" -f2 | cut -d"p" -f1)
 
 # Command line switches: https://peter.sh/experiments/chromium-command-line-switches/
 # - Review and add custom flags in: /etc/chromium.d
-CHROMIUM_OPTS="--kiosk --window-size=${RES_X:-1280},${RES_Y:-720} --window-position=0,0"
+CHROMIUM_OPTS="--no-sandbox --kiosk --window-size=${RES_X:-1280},${RES_Y:-720} --window-position=0,0"
 
 # If you want tablet mode, uncomment the next line.
 #CHROMIUM_OPTS+=' --force-tablet-mode --tablet-ui'
